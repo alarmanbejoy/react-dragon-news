d3bb393c10abfb33042fcfa1de451d1a09f676b9
alarmanbejoy <alarmanbejoy@gmail.com>
2024-09-24 21:19:26 +0600

    done

--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
     <li><NavLink to='/about'>About</NavLink></li>
     <li><NavLink to='/career'>Career</NavLink></li>
     <li><NavLink to='/login'>Login</NavLink></li>
+    
     </>
     
      
